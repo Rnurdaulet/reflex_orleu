5b015763f93548b5025b7074d925ad8109300b57
Rnurdaulet <rakhymberdin@gmail.com>
2025-04-16 13:29:29 +0500

    CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -116,23 +116,24 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # для collectstatic
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # куда collectstatic будет собирать
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # откуда collectstatic будет брать файлы
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://reflex.odx.kz",  # Добавь твой домен
+]
+
+
+
 
 
 MEDIA_URL = '/media/'
